5280a0898559eabb1fee1d03f37406539ca75964
Lenhazada <gabriellenhardtcontato@gmail.com>
2025-06-17 19:24:45 -0300

    Adding fps Adding text on screen

--- code/menu.py
+++ code/menu.py
@@ -10,7 +10,7 @@ from code.const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE, COLOR_
 class Menu:
     def __init__(self, window):
         self.window = window
-        self.surf = pygame.image.load('./asset/MenuBg.png')  # search and load the image
+        self.surf = pygame.image.load('./asset/MenuBg.png').convert_alpha()  # search and load the image
         self.rect = self.surf.get_rect(left=0, top=0)  # create an invisible rect
 
     def run(self):
